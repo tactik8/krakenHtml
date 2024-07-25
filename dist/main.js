ab75784dbefaed358ff89356ffce2e503a5edd83
tactik8 <48621231+tactik8@users.noreply.github.com>
2024-07-25 19:11:28 +0000

    go

--- dist/main.js
+++ dist/main.js
@@ -170,7 +170,7 @@ function $09aaf31e9efdd809$export$65e8537a85f61405(path, options) {
     return $09aaf31e9efdd809$var$_getHtmlUrl(path, options);
 }
 function $09aaf31e9efdd809$var$_getHtmlUrl(path, options) {
-    let domain = options?.hostname || null;
+    let domain = 'https://' + options?.hostname || null;
     if (!domain || domain == null) domain = "https://www.test.com";
     let url = new URL(domain);
     // Do params
